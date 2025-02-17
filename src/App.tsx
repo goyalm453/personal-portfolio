e824544e6e38bcd077aadebf9a0b18bc0ad2dc39
Mohit Goyal <goyalm453@gmail.com>
2025-02-18 00:20:36 +0530

    scroll changes

--- src/App.tsx
+++ src/App.tsx
@@ -173,8 +173,8 @@ function App() {
       number: "01",
       title: "Resumer- AI-Powered Resume Builder",
       description: "Resumer is an Ai Powered Resume Builder toolkit that helps job seekers and Working Professionals to create ATS-friendly, professional resumes effortlessly. With AI-powered optimization, smart templates, real-time suggestions, and one-click customization, users can craft standout resumes in seconds. 🚀",
-      image: "/Resumer.png",
-      mockup: "/Resumer.png",
+      image: "pubic/Resumer.png",
+      mockup: "pubic/Resumer.png",
       projectUrl: "https://www.behance.net/gallery/205453139/Resumer-AI-Powered-Resume-Builder-Toolkit-Case-Study"
     },
     {
